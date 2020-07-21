2a0a089f2417671ff90608be8e270cbf756b3196
tw2686 <tw2686@github.com>
2020-07-21 10:06:05 -0400

    Rename project

--- atm-console_1.1/src/main/java/com/smbcgroup/training/atm/AccountNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.smbcgroup.training.atm;
-
-public class AccountNotFoundException extends Exception {
-
-	public String toString() {
-		return "Account Not Found.";
-	}
-}
--- atm-console_1.1/src/main/java/com/smbcgroup/training/atm/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.smbcgroup.training.atm;
-
-public class UserNotFoundException extends Exception{
-	
-	public String toString() {
-		return ("User Not Found.");
-	}
-	
-}
