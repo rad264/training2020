1bf2ad2202dabd83bf1cf0986f6b6f8acfb3b73d
Ashton <ashton@localhost>
2020-07-27 14:07:02 -0400

    Tests for /account/{accountBumber} with use of matchers

--- atm/atm-api/src/test/java/com/smbcgroup/training/atm/api/APIControllerTest.java
+++ atm/atm-api/src/test/java/com/smbcgroup/training/atm/api/APIControllerTest.java
@@ -2,15 +2,22 @@ package com.smbcgroup.training.atm.api;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
+
+import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.smbcgroup.training.atm.ATMService;
+import com.smbcgroup.training.atm.Account;
 import com.smbcgroup.training.atm.User;
+import com.smbcgroup.training.atm.dao.AccountNotFoundException;
 import com.smbcgroup.training.atm.dao.UserNotFoundException;
 
 public class APIControllerTest {
@@ -49,6 +56,34 @@ public class APIControllerTest {
 		MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/users/rdelaney")).andReturn();
 		assertEquals(200, result.getResponse().getStatus());
 		assertEquals("{\"userId\":\"rdelaney\",\"accounts\":[\"123456\"]}", result.getResponse().getContentAsString());
+	}
+
+	@Test
+	public void getAccount_NotFound() throws Exception {
+		APIController.service = new ATMService(null) {
+			@Override
+			public Account getAccount(String accountNumber) throws AccountNotFoundException {
+				throw new AccountNotFoundException();
+			}
+		};
+		mockMvc.perform(MockMvcRequestBuilders.get("/accounts/123457"))
+				.andExpect(MockMvcResultMatchers.status().is(404));
+	}
+
+	@Test
+	public void getAccount_Found() throws Exception {
+		APIController.service = new ATMService(null) {
+			@Override
+			public Account getAccount(String accountNumber) throws AccountNotFoundException {
+				Account account = new Account();
+				account.setAccountNumber("123456");
+				account.setBalance(new BigDecimal("100.00"));
+				return account;
+			}
+		};
+		mockMvc.perform(MockMvcRequestBuilders.get("/accounts/123456"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.accountNumber", CoreMatchers.is("123456")))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.balance", CoreMatchers.is(100.0)));
 	}
 
 }
