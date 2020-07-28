2e8ecfca403d4b2ca4a844de40c8b554d35312ad
laynebritton <laynebritton1998@gmail.com>
2020-07-28 14:11:23 -0600

    Fixed init db

--- atm/atm-dao-jpa-impl/src/main/java/com/smbcgroup/training/atm/dao/jpa/InitDB.java
+++ atm/atm-dao-jpa-impl/src/main/java/com/smbcgroup/training/atm/dao/jpa/InitDB.java
@@ -8,6 +8,13 @@ public class InitDB {
 
 	public static void main(String[] args) throws ClassNotFoundException {
 		EntityManager em = new AccountJPAImpl().emf.createEntityManager();
+		
+		em.getTransaction().begin();
+	    em.createQuery("DELETE FROM HistoryEntity").executeUpdate();
+	    em.createQuery("DELETE FROM AccountEntity").executeUpdate();
+	    em.createQuery("DELETE FROM UserEntity").executeUpdate();
+		em.getTransaction().commit();
+		
 		em.getTransaction().begin();
 
 		UserEntity rdelaney = new UserEntity("rdelaney");
@@ -16,7 +23,7 @@ public class InitDB {
 
 		UserEntity l = new UserEntity("l");
 		em.persist(l);
-		em.merge(new AccountEntity("123456", new BigDecimal("100"), l));
+		em.merge(new AccountEntity("654321", new BigDecimal("100"), l));
 		
 		em.getTransaction().commit();
 		em.close();
