6819ae96ffa4cfcf35f32bcee9e1392f1d397c96
laynebritton <laynebritton1998@gmail.com>
2020-07-31 09:45:36 -0600

    Valid move checking implemented. Needs to be cleaned up

--- js-arcade/othello/Othello.js
+++ js-arcade/othello/Othello.js
@@ -23,11 +23,200 @@ function Othello() {
 
 
     }
-    this.initialize()
+    this.presentValidMoves = function() {
+        for(m = 0; m < width; m++){
+            for(n = 0; n < height; n++){
+                if(this.isAValidMove(m,n) === true){
+                    gameBoard.getButton(m,n).innerHTML = "*"
+                }else{
+                    if( gameBoard.getButton(m,n).innerHTML === "*"){
+                        gameBoard.getButton(m,n).innerHTML = ""
+                    }
+                }
+            }
+        }
 
+        return false;
+    }
+
+    this.isAValidMove = function (x, y) {
+
+
+
+        if(this.squares[x][y] !== undefined){
+            return false
+        }
+
+        let current_color = this.blackIsNext ? black_marker : white_marker;
+        let buttons = []
+        // check right
+        for (i = x + 1; i < width; i++) {
+            if (this.squares[i][y] === undefined) {
+                break
+            }
+            if (this.squares[i][y] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(i, y), x: i, y: y })
+
+            }
+            if (this.squares[i][y] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+        }
+
+        buttons = []
+        // check left
+        for (i = x - 1; i >= 0; i--) {
+            if (this.squares[i][y] === undefined) {
+                break
+            }
+            if (this.squares[i][y] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(i, y), x: i, y: y })
+
+            }
+            if (this.squares[i][y] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+        }
+
+        buttons = []
+        // check up
+        for (i = y - 1; i >= 0; i--) {
+            if (this.squares[x][i] === undefined) {
+                break
+            }
+            if (this.squares[x][i] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(x, i), x: x, y: i })
+
+            }
+            if (this.squares[x][i] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+        }
+
+        buttons = []
+        // check down
+        for (i = y + 1; i < height; i++) {
+            if (this.squares[x][i] === undefined) {
+                break
+            }
+            if (this.squares[x][i] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(x, i), x: x, y: i })
+
+            }
+            if (this.squares[x][i] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+        }
+
+
+        buttons = []
+        // check diagonal - down-right
+        x_iterator = x + 1
+        y_iterator = y + 1
+        while(x_iterator < width && y_iterator < height){
+            if (this.squares[x_iterator][y_iterator] === undefined) {
+                break
+            }
+            if (this.squares[x_iterator][y_iterator] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(x_iterator, y_iterator), x: x_iterator, y: y_iterator })
+
+            }
+            if (this.squares[x_iterator][y_iterator] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+            x_iterator++
+            y_iterator++
+        }
+
+        buttons = []
+        // check diagonal - down-left
+        x_iterator = x - 1
+        y_iterator = y + 1
+        while(x_iterator >= 0 && y_iterator < height){
+            if (this.squares[x_iterator][y_iterator] === undefined) {
+                break
+            }
+            if (this.squares[x_iterator][y_iterator] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(x_iterator, y_iterator), x: x_iterator, y: y_iterator })
+
+            }
+            if (this.squares[x_iterator][y_iterator] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+            x_iterator--
+            y_iterator++
+        }
+
+        buttons = []
+        // check diagonal - up-right
+        x_iterator = x + 1
+        y_iterator = y - 1
+        while(x_iterator < width && y_iterator >= 0){
+            if (this.squares[x_iterator][y_iterator] === undefined) {
+                break
+            }
+            if (this.squares[x_iterator][y_iterator] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(x_iterator, y_iterator), x: x_iterator, y: y_iterator })
+
+            }
+            if (this.squares[x_iterator][y_iterator] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+            x_iterator++
+            y_iterator--
+        }
+
+        buttons = []
+        // check diagonal - up-left
+        x_iterator = x - 1
+        y_iterator = y - 1
+        while(x_iterator >= 0 && y_iterator >= 0){
+            if (this.squares[x_iterator][y_iterator] === undefined) {
+                break
+            }
+            if (this.squares[x_iterator][y_iterator] !== current_color) {
+                buttons.push({ button: gameBoard.getButton(x_iterator, y_iterator), x: x_iterator, y: y_iterator })
+
+            }
+            if (this.squares[x_iterator][y_iterator] === current_color) {
+                if(buttons.length > 0){
+                    return true
+                }
+                break
+            }
+            x_iterator--
+            y_iterator--
+        }
+
+        return false
+        
+    }
+    this.initialize()
+    this.presentValidMoves()
 
     this.click = function (x, y) {
-        if (!this.gameOver && (this.squares[x][y] === undefined)) {
+        if (!this.gameOver && (this.squares[x][y] === undefined) && gameBoard.getButton(x,y).innerHTML === "*") {
             var button = gameBoard.getButton(x, y);
             button.innerHTML = this.blackIsNext ? black_marker : white_marker;
             this.squares[x][y] = this.blackIsNext ? black_marker : white_marker;
@@ -39,13 +228,18 @@ function Othello() {
                 alert((this.blackIsNext ? "Black" : "White") + " wins!");
             }
             this.blackIsNext = !this.blackIsNext;
+            this.presentValidMoves()
+
         }
     };
+
+
     this.isGameOver = function (x, y) {
 
-
-
     }
+
+
+
     this.checkForFlips = function (x, y) {
         let current_color = this.blackIsNext ? black_marker : white_marker;
         let buttons = []
