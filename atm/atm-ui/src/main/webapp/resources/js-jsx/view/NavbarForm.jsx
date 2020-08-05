e834f1d4704250a215771acc90e9d671769e3f63
laynebritton <laynebritton1998@gmail.com>
2020-08-05 15:02:20 -0600

    Navbar styling

--- atm/atm-ui/src/main/webapp/resources/js-jsx/view/NavbarForm.jsx
+++ atm/atm-ui/src/main/webapp/resources/js-jsx/view/NavbarForm.jsx
@@ -2,21 +2,24 @@ function NavbarForm() {
     return (
         <div>
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">ATM</a>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+                <a class="navbar-brand" href="#">SMBC ATM</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <li class="nav-item active" >
+                            <a class="nav-link" role="button">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" role="button">Deposit</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link" role="button">Withdraw</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" role="button">Transfer</a>
                         </li>
                     </ul>
 
