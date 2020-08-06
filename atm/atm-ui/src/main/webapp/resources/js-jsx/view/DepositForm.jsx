1571b071e87c2ae5c07ad39fbc4eff7ac1abd609
laynebritton <laynebritton1998@gmail.com>
2020-08-06 14:40:24 -0600

    Withdraw in card. Deposit card cleaned up

--- atm/atm-ui/src/main/webapp/resources/js-jsx/view/DepositForm.jsx
+++ atm/atm-ui/src/main/webapp/resources/js-jsx/view/DepositForm.jsx
@@ -18,13 +18,3 @@ function DepositForm(props) {
 
     )
 }
-
-{/* <div class="form-group col-md-6">
-<h3 class="card-title">Check Balance</h3>
-<label for="accountNumber">Account Number:</label>
-<input placeholder="e.g. 123456" type="text" class="form-control" name="accountNumber" onChange={props.onChange} value={props.accountNumber} />
-</div>
-<div class="col-md-6">
-<button class="btn btn-primary" onClick={props.onClick}>Check Balance</button>
-
-</div> */}
--- atm/atm-ui/src/main/webapp/resources/js-jsx/view/WithdrawForm.jsx
+++ atm/atm-ui/src/main/webapp/resources/js-jsx/view/WithdrawForm.jsx
@@ -1,15 +1,20 @@
 function WithdrawForm(props) {
     return (
-        <div>
-            <h3>Withdrawal</h3>
-            <label for="accountNumber">Account number:</label>
-            <input type="text" name="accountNumber" onChange={props.onChange} value={props.accountNumber} ></input>
-            <br></br>
-            <label for="withdrawAmount">Withdraw amount:</label>
-            <input type="text" name="withdrawAmount" onChange={props.onChange} value={props.withdrawAmount} ></input>
-            <button onClick={props.onClick}>Withdraw</button>
+        <div class="col-md-4 card bank-function-card">
+            <div class="form-group">
+                <h3 class="card-title">Withdrawal</h3>
+                <label for="accountNumber">Account number:</label>
+                <input class="form-control" placeholder="e.g. 123456" type="text" name="accountNumber" onChange={props.onChange} value={props.accountNumber} ></input>
+                <br></br>
+                <label for="withdrawAmount">Withdraw amount:</label>
+                <input class="form-control" placeholder="e.g. 10.50" type="text" name="withdrawAmount" onChange={props.onChange} value={props.withdrawAmount} ></input>
+            </div>
+            <div class="">
+            <button class="btn btn-primary" onClick={props.onClick}>Withdraw</button>
 
+            </div>
         </div>
+
 
     )
 }
