b14ea67b8d976291ed0006a89cb96fd9a81b4885
laynebritton <laynebritton1998@gmail.com>
2020-08-06 14:26:45 -0600

    Redid form for checkbalance

--- atm/atm-ui/src/main/webapp/resources/js-jsx/view/Balance.jsx
+++ atm/atm-ui/src/main/webapp/resources/js-jsx/view/Balance.jsx
@@ -2,5 +2,11 @@ function Balance(props) {
     let error;
     if (props.statusCode && props.statusCode !== 200)
         error = props.statusCode === 404 ? "Account Not Found" : "Unexpected Error";
-    return <p>Balance: <span style={{color: error ? "red" : "green"}}>{error || props.balance}</span></p>;
+    return (
+        <div class="col-md-6">
+            <div class="card-body">
+                <p>Balance: <span style={{ color: error ? "red" : "green" }}>{error || props.balance}</span></p>
+            </div>
+        </div>
+    );
 }
--- atm/atm-ui/src/main/webapp/resources/js-jsx/view/CheckBalanceForm.jsx
+++ atm/atm-ui/src/main/webapp/resources/js-jsx/view/CheckBalanceForm.jsx
@@ -1,14 +1,18 @@
 function CheckBalanceForm(props) {
     return (
         <form>
+            <br></br>
             <div class="form-group col-md-6">
                 <h3 class="card-title">Check Balance</h3>
-                <label for="accountNumber">AccountNumber:</label>
+                <label for="accountNumber">Account Number:</label>
                 <input placeholder="e.g. 123456" type="text" class="form-control" name="accountNumber" onChange={props.onChange} value={props.accountNumber} />
-                <button onClick={props.onClick}>Check Balance</button>
-
+            </div>
+            <div class="col-md-6">
+                <button class="btn btn-primary" onClick={props.onClick}>Check Balance</button>
 
             </div>
+
+
         </form>
 
     );
