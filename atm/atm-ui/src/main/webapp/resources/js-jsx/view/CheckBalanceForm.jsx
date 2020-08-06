822d593cb8ff0360672bc957cb95dedf22e8e1f8
laynebritton <laynebritton1998@gmail.com>
2020-08-06 14:45:26 -0600

    Removed comments

--- atm/atm-ui/src/main/webapp/resources/js-jsx/view/CheckBalanceForm.jsx
+++ atm/atm-ui/src/main/webapp/resources/js-jsx/view/CheckBalanceForm.jsx
@@ -17,13 +17,3 @@ function CheckBalanceForm(props) {
 
     );
 }
-
-
-{/* <div>
-<h3>Check Balance</h3>
-<label for="accountNumber">AccountNumber:</label>
-<input type="text" name="accountNumber" onChange={props.onChange} value={props.accountNumber} />
-<button onClick={props.onClick}>Check Balance</button>
-
-
-</div> */}
