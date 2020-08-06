74c0428e7f73cf37b8ac1c8d1fe7aab486ef374e
laynebritton <laynebritton1998@gmail.com>
2020-08-06 14:21:03 -0600

    Initial spinner while page loads

--- atm/atm-ui/src/main/webapp/resources/js-jsx/view/CheckBalanceForm.jsx
+++ atm/atm-ui/src/main/webapp/resources/js-jsx/view/CheckBalanceForm.jsx
@@ -1,10 +1,25 @@
 function CheckBalanceForm(props) {
     return (
-        <div>
-            <h3>Check Balance</h3>
-            <label for="accountNumber">AccountNumber:</label>
-            <input type="text" name="accountNumber" onChange={props.onChange} value={props.accountNumber} />
-            <button onClick={props.onClick}>Check Balance</button>
-        </div>
+        <form>
+            <div class="form-group col-md-6">
+                <h3 class="card-title">Check Balance</h3>
+                <label for="accountNumber">AccountNumber:</label>
+                <input placeholder="e.g. 123456" type="text" class="form-control" name="accountNumber" onChange={props.onChange} value={props.accountNumber} />
+                <button onClick={props.onClick}>Check Balance</button>
+
+
+            </div>
+        </form>
+
     );
 }
+
+
+{/* <div>
+<h3>Check Balance</h3>
+<label for="accountNumber">AccountNumber:</label>
+<input type="text" name="accountNumber" onChange={props.onChange} value={props.accountNumber} />
+<button onClick={props.onClick}>Check Balance</button>
+
+
+</div> */}
