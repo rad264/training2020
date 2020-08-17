1ec1b3e5d40897313f7c573b2d444696f973ac72
evansyatich <eyatich@seas.upenn.edu>
2020-08-17 10:17:20 -0400

    bounce added

--- js-arcade/Connect4Game/js/connectfourMain.js
+++ js-arcade/Connect4Game/js/connectfourMain.js
@@ -41,25 +41,25 @@ function resetGame (rows, columns, destination) {
         resetNotClicked = false;
         output.className = "none";
         
-        while (destination.firstChild) // While the destination has children
-            destination.removeChild(destination.firstChild); // Clear the previous tables
+        while (destination.firstChild) 
+            destination.removeChild(destination.firstChild); 
         
-        while (board.firstChild) // While the board has children
-            board.removeChild(board.firstChild); // Clear the previous tables
+        while (board.firstChild) 
+            board.removeChild(board.firstChild); 
         
-        /* Initialize array */
+       
         array = new Array(rows);
         for (var i = 0; i < rows; i++) {
             array[i] = new Array(columns);
             
-            /* set all elements to zero */
+           
             for (var j = 0; j < columns; j++) {
                 array[i][j] = 0;
             }
         }
         
         currentIndexOfCol = new Array(columns);
-        /* set all elements to zero */
+        
         for (var j = 0; j < columns; j++) {
              currentIndexOfCol[j] = 0;
         }
@@ -70,7 +70,7 @@ function resetGame (rows, columns, destination) {
             var tr = document.createElement('tr');
                 for (var j = columns ; j>0 ; j--){
                     var td = document.createElement('td');
-                    td.id = (i-1) + "" + (-j+7); // (-j+7) to fix the order (to have td '00' being the bottom-left one)
+                    td.id = (i-1) + "" + (-j+7); 
                     td.onclick = tdClicked;
                     td.onmouseover = tdMouseOver;
                     td.onmouseout = tdMouseOut;
@@ -85,7 +85,7 @@ function resetGame (rows, columns, destination) {
             var tr = document.createElement('tr');
                 for (var j = columns ; j>0 ; j--){
                     var td = document.createElement('td');
-                    td.id = (i-1) + "" + (-j+7)+"b"; // (-j+8) to fix the order (to have td '00' being the bottom-left one)
+                    td.id = (i-1) + "" + (-j+7)+"b"; 
                     td.onclick = tdClicked;
                     td.onmouseover = tdMouseOver;
                     td.onmouseout = tdMouseOut;
@@ -94,13 +94,7 @@ function resetGame (rows, columns, destination) {
             table2.appendChild(tr);
         }
         board.appendChild(table2);
-        
-        /*
-        context.clearRect( 0 , 0 , canvas.width , canvas.height); // Clear canvas
-        canvas.width = canvas.width; // Clear canvas
-        */
-        
-        /* Remove the current canvas then create a new one and insert it */
+       
         canvas.parentNode.removeChild(canvas);
         canvas = document.createElement('canvas');
         canvas.width = "585";
@@ -110,7 +104,7 @@ function resetGame (rows, columns, destination) {
         context = canvas.getContext('2d');
         
         role=0;
-        setTimeout(function(){gameRunning = true;resetNotClicked = true;},100); // To handle clearing canvas issues
+        setTimeout(function(){gameRunning = true;resetNotClicked = true;},100); 
         output.innerHTML = "Turn: Player " + ((role%2)+1);
     }
 }
@@ -141,12 +135,12 @@ function tdMouseOut (){
 function tdClicked () {
     if (gameRunning) {
         var col = this.id.charAt(1);
-        if (currentIndexOfCol[col]>=6) {// if the column is full
+        if (currentIndexOfCol[col]>=6) {
             output.innerHTML = "Column is full";
         }
         else {
             dropDisk(col,(role++%2)+1);
-            /* Clear the white */
+            
             if (gameRunning){
                 var td;
                 for (i=(rows-1) ; i>=0 ;i--){
@@ -163,46 +157,46 @@ function tdClicked () {
 function dropDisk (col, player){
     gameRunning = true;
     var row = currentIndexOfCol[col]++;
-    var td = document.getElementById(""+row+col); // to make it string
+    var td = document.getElementById(""+row+col); 
     
     var fieldX = Math.round( (cellWidth+0.5)*col );
     var fieldY = 0;
     var fieldWidth = cellWidth;
     var fieldHeight = Math.round((6-row)*(cellWidth));
         
-    if (player==1) { // Player 1  
+    if (player==1) { 
         array[row][col] = 1 ;                
         diskColor = "rgb(226, 65, 65)";
         animateDroppingDisk(fieldX, fieldY, fieldWidth, fieldHeight, diskColor); 
     }
-    else { // Player 2
+    else { 
         array[row][col] = 2 ;
         diskColor = "rgb(248, 210, 52)";
         animateDroppingDisk(fieldX, fieldY, fieldWidth, fieldHeight, diskColor); 
     }
     output.innerHTML = "Turn: Player " + ((role%2)+1);
 
-    if (isAWinningMove(row, col)) { // If this was a winning move
-        gameRunning=false; // Stop game
+    if (isAWinningMove(row, col)) { 
+        gameRunning=false; 
         
-        /* Clear the highlighting white */
+        
         for (i=(rows-1) ; i>=0 ;i--){
             td = document.getElementById(i+""+col);
             td.style.background = "none";
         }
         
-        /* Mark the winning cells */
+        
         for (var m=0 ; m < winningMoveCells.length ; m++){
             td = document.getElementById(""+winningMoveCells[m]);
             td.style.background = "rgba(0,0,0,0.4)";
         }
     
-        if (((role-1)%2 +1) == 1 ) { // Player 1 won
+        if (((role-1)%2 +1) == 1 ) { 
             output.innerHTML = "Player 1 WON!";
             output.className = "player1";
             player1ScoreOutput.innerHTML = ++player1Score;
         }
-        else { // Player 2 won
+        else { 
             output.innerHTML = "Player 2 WON!";
             output.className = "player2";
             player2ScoreOutput.innerHTML = ++player2Score;
@@ -223,7 +217,7 @@ function gridIsFull() {
     return (isFull);
 }
 
-function accessArray(row, col){ // This function is created seperately to handle the case of having invalid input to the array
+function accessArray(row, col){ 
     try {
         var val = array[row][col];
         if (val==undefined)
@@ -231,7 +225,7 @@ function accessArray(row, col){ // This function is created seperately to handle
         return val;
     }
     catch(err) {
-        return 0; // Not applicable (out of range)
+        return 0;
     }
 }
 
