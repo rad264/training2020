7cb5eaf2c09aff5f2a5e16378dc013ce7bfed0a0
Patricia Kusumaningtyas <patricia.adiyoso@gmail.com>
2020-08-25 09:58:47 -0400

    Added local version of ATM.java

--- atm/atm-console/src/main/java/com/smbcgroup/training/atm/console/ATM.java
+++ atm/atm-console/src/main/java/com/smbcgroup/training/atm/console/ATM.java
@@ -1,57 +1,36 @@
-package com.smbcgroup.training.atm.console;
+package com.smbcgroup.training.atm;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-
-import org.apache.wink.client.ClientConfig;
-import org.apache.wink.client.ClientWebException;
-import org.apache.wink.client.RestClient;
-
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import com.smbcgroup.training.atm.ATMServiceException;
-import com.smbcgroup.training.atm.ATMServiceException.Type;
-import com.smbcgroup.training.atm.Account;
-import com.smbcgroup.training.atm.User;
 
 public class ATM {
+
 	public static void main(String[] args) throws IOException {
 		new ATM(System.in, System.out).beginSession();
 	}
 
 	private static enum Action {
-		login, changeAccount, checkBalance, deposit, withdraw;
-		// TODO: add more actions
+		login, 
+		changeAccount, 
+		checkBalance, 
+		createAccount, 
+		deposit, 
+		withdraw, 
+		transferBetweenAccounts,
+		viewAccountsSummary, 
+		viewAccountActivity;
 	}
 
-	private static final String API_ROOT_URL = "http://localhost:8080/atm-api/";
-
-	private RestClient restClient;
 	private BufferedReader inputReader;
 	private PrintStream output;
 	private String loggedInUser;
-	private String[] loggedInUserAccounts;
 	private String selectedAccount;
 	private Action selectedAction = Action.login;
 
 	private ATM(InputStream input, PrintStream output) {
-		Application restClientApp = new Application() {
-			@Override
-			public Set<Object> getSingletons() {
-				HashSet<Object> set = new HashSet<Object>();
-				set.add(new JacksonJsonProvider());
-				return set;
-			}
-		};
-		this.restClient = new RestClient(new ClientConfig().applications(restClientApp));
 		this.inputReader = new BufferedReader(new InputStreamReader(input));
 		this.output = output;
 	}
@@ -85,7 +64,9 @@ public class ATM {
 
 	private boolean promptUserInput() {
 		if (selectedAction == null) {
-			output.println("What would you like to do?");
+			output.println("Available actions:");
+			for (Action a: Action.values()) output.println("> "+a);
+			output.println("> exit\nWhat would you like to do?");
 			return true;
 		}
 		switch (selectedAction) {
@@ -93,19 +74,26 @@ public class ATM {
 			output.println("Enter user ID:");
 			return true;
 		case changeAccount:
-			output.println("Enter account number: (" + String.join(", ", loggedInUserAccounts) + ")");
+			output.println(
+					"Enter account number: (" + String.join(", ", AccountAccessor.getUserAccounts(loggedInUser)) + ")");
 			return true;
 		case deposit:
-		case withdraw:
-			output.println("Enter amount:");
+			output.println("How much money do you want to deposit?");
 			return true;
-		// TODO: prompts for other actions(?)
+		case withdraw:
+			output.println("How much money do you want to withdraw?");
+			return true;
+		case transferBetweenAccounts:
+			output.println(
+					"Which account do you want to transfer to? (" 
+					+ (String.join(", ", AccountAccessor.getUserAccounts(loggedInUser))).replace(selectedAccount, "") + ")");
+			return true;
 		default:
 			return false;
 		}
 	}
 
-	private Action performActionAndGetNextAction(String input) throws ATMException, SystemExit {
+	private Action performActionAndGetNextAction(String input) throws ATMException, SystemExit, IOException {
 		if ("exit".equals(input))
 			throw new SystemExit();
 		if (selectedAction == null) {
@@ -118,18 +106,16 @@ public class ATM {
 		switch (selectedAction) {
 		case login:
 			try {
-				User user = restClient.resource(API_ROOT_URL + "users/" + input).accept(MediaType.APPLICATION_JSON_TYPE)
-						.get(User.class);
-				loggedInUserAccounts = user.getAccounts();
+				AccountAccessor.getUserAccounts(input);
 				loggedInUser = input;
 				return Action.changeAccount;
-			} catch (ClientWebException e) {
+			} catch (Exception e) {
 				throw new ATMException("Invalid user ID.");
 			}
 		case changeAccount:
 			if (!input.matches("^\\d{6}$"))
 				throw new ATMException("Invalid account number.");
-			for (String userAccount : loggedInUserAccounts) {
+			for (String userAccount : AccountAccessor.getUserAccounts(loggedInUser)) {
 				if (userAccount.equals(input)) {
 					selectedAccount = input;
 					return null;
@@ -137,70 +123,100 @@ public class ATM {
 			}
 			throw new ATMException("Account number not found.");
 		case checkBalance:
-			try {
-				Account account = restClient.resource(API_ROOT_URL + "accounts/" + selectedAccount)
-						.accept(MediaType.APPLICATION_JSON_TYPE).get(Account.class);
-				output.println("Balance: $" + account.getBalance());
-			} catch (ClientWebException e) {
-				throw new RuntimeException(e);
-			}
+			output.println("Balance: $" + AccountAccessor.getAccountBalance(selectedAccount) + "\n");
+			break;
+		case createAccount:
+			output.println(
+					"Created new account with number " + AccountAccessor.createAccount(loggedInUser) + " and balance $0.00\n");
 			break;
 		case deposit:
-//			try {
-//				service.deposit(selectedAccount, convertToBigDecimal(input));
-//			} catch (ATMServiceException e) {
-//				throw new ATMException(e);
-//			} catch (AccountNotFoundException e) {
-//				throw new RuntimeException(e);
-//			}
+			input = checkDecimalInput(input);
+			if (!(AccountAccessor.deposit(selectedAccount, input))) {
+				output.println("Invalid input");
+				return null;
+			} else {
+				output.println(
+						"Deposited $" + input + " to account " + selectedAccount 
+						+ ".\nCurrent balance = $" + AccountAccessor.getAccountBalance(selectedAccount) + "\n");
+			}
 			break;
 		case withdraw:
-//			try {
-//				service.withdraw(selectedAccount, convertToBigDecimal(input));
-//			} catch (ATMServiceException e) {
-//				throw new ATMException(e);
-//			} catch (AccountNotFoundException e) {
-//				throw new RuntimeException(e);
-//			}
+			input = checkDecimalInput(input);
+			if (!isPossibleToWithdraw(input)) break;
+			if (!(AccountAccessor.withdraw(selectedAccount, input))) {
+				output.println("Invalid number");
+				return null;
+			} else {
+				output.println(
+						"Withdrew $" + input + " from account " + selectedAccount 
+						+ ".\nCurrent balance = $" + AccountAccessor.getAccountBalance(selectedAccount) + "\n");
+			}
 			break;
-		// TODO: handle other actions
+		case transferBetweenAccounts:
+			if (selectedAccount.equals(input) ) {
+				output.println("Selected account cannot be your current account");
+				return null;
+			}
+			for (String userAccount : AccountAccessor.getUserAccounts(loggedInUser)) {
+				if (userAccount.equals(input)) {
+					output.println("How much money do you want to transfer?");
+					String amount = inputReader.readLine();
+					amount = checkDecimalInput(amount);
+					if (!isPossibleToWithdraw(amount)) break;
+					AccountAccessor.transfer(selectedAccount, input, amount);
+					output.println("Successfully transferred $" + amount + " from " + selectedAccount +" to " + input 
+							+ ".\nCurrent balance for " + selectedAccount + " is $" + AccountAccessor.getAccountBalance(selectedAccount)
+							+ ".\nCurrent balance for " + input + " is $" + AccountAccessor.getAccountBalance(input) + ".\n");
+					return null;
+				}
+			}
+			throw new ATMException("Invalid input.");
+		case viewAccountsSummary:
+			for (String userAccount : AccountAccessor.getUserAccounts(loggedInUser)) {
+				output.println("Account #" + userAccount + " | Balance: $" + AccountAccessor.getAccountBalance(userAccount));
+			}
+			output.println("\n");
+			break;
+		case viewAccountActivity:
+			output.println(AccountAccessor.getAccountActivity(selectedAccount)+"\n");
+			break;
 		}
 		return null;
 	}
-
-	private BigDecimal convertToBigDecimal(String input) throws ATMException {
-		try {
-			return new BigDecimal(input);
-		} catch (NumberFormatException e) {
-			throw new ATMException("Invalid amount.");
+	
+	private String checkDecimalInput(String amount) {
+		if (amount.indexOf(".") == -1) amount = amount + ".00";
+		return amount;
+	}
+	
+	private boolean isPossibleToWithdraw(String input) {
+		switch (AccountAccessor.withdrawSuccess(selectedAccount, input)) {
+		case "True":
+			return true;
+		case "False":
+			output.println(
+					"Sorry, we can't withdraw your entered amount due to insufficient funds.");
+			return false;
+		case "Error":
+			output.println(
+					"Invalid input.");
+			return false;
+		default:
+			return false;
 		}
 	}
 
-	private static class SystemExit extends Throwable {
+	private class SystemExit extends Throwable {
 		private static final long serialVersionUID = 1L;
 	}
 
-	private static class ATMException extends Exception {
+	public class ATMException extends Exception {
 		private static final long serialVersionUID = 1L;
 
 		public ATMException(String message) {
 			super(message);
 		}
-
-		public ATMException(ATMServiceException e) {
-			super(determineErrorMessage(e.getType()));
-		}
-
-		private static String determineErrorMessage(Type type) {
-			switch (type) {
-			case NON_POSITIVE_AMOUNT:
-				return "Amount must be greater than 0.";
-			case INSUFFICIENT_FUNDS:
-				return "Insufficient funds.";
-			}
-			return "Unexpected error.";
-		}
-
 	}
 
 }
+
