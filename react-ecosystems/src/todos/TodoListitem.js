dafcf2d827a07d38ce7730592c02b20bdeca1580
tw2686 <tw2686@github.com>
2020-08-31 13:39:54 -0400

    Add redux, thunk to ecosystem

---
+++ react-ecosystems/src/todos/TodoListitem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./TodoListitem.css";
+
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
+    <div className="todo-item-container">
+        <h3>{todo.text}</h3>
+        <div>
+            <button
+                onClick={() => onCompletedPressed(todo.text)}
+                className="completed-button"
+            >
+                Mark As Completed
+            </button>
+            <button
+                onClick={() => onRemovePressed(todo.text)}
+                className="remove-button"
+            >
+                Remove
+            </button>
+        </div>
+    </div>
+);
+
+export default TodoListItem;
