abccc8977b5f44a7ea24870b877781b20b3e267b
Patricia Kusumaningtyas <patricia.adiyoso@gmail.com>
2020-09-24 14:54:15 -0400

    Merged files, stylized page, and added solution

--- js-arcade/sudoku/GameBoardSudoku.js
+++ js-arcade/sudoku/GameBoardSudoku.js
@@ -1,17 +1,19 @@
-function GameBoardSudoku(containerNode, sudokuString, handleClick) {
+function GameBoardSudoku(containerNode, sudokuString, sudokuSolution, handleClick) {
     var buttons = [];
     for (let i = 0; i < 9; i++)
         buttons.push([]);
     containerNode.innerHTML = "";
     var table = document.createElement("table");
-    var count = 1;
+    table.className = "board";
+    var count = 0;
     for (var y = 0; y < 9; y++) {
         var row = document.createElement("tr");
         for (var x = 0; x < 9; x++) {
             var cell = document.createElement("td");
             cell.className = "board_cell";
             if (sudokuString.charAt(count) == "0") {
-                var button = document.createElement("button");
+                var button = document.createElement("p");
+                button.className = "button";
                 button.state = 0;
                 button.innerHTML = button.state.toString();
                 button.onclick = (function(x, y) {
@@ -23,6 +25,7 @@ function GameBoardSudoku(containerNode, sudokuString, handleClick) {
                 cell.appendChild(button);
             } else {
                 var number = document.createElement("p");
+                number.className = "number";
                 number.state = sudokuString.charAt(count);
                 number.innerHTML = sudokuString.charAt(count).toString();
                 buttons[x][y] = number;
@@ -34,6 +37,37 @@ function GameBoardSudoku(containerNode, sudokuString, handleClick) {
         table.appendChild(row);
     }
     containerNode.appendChild(table);
+    var solutionArea = document.getElementById("solutionArea");
+    [1, 2, 3].forEach(function () {
+        solutionArea.appendChild(document.createElement("br"));
+    })
+    var solutionButton = document.createElement("button");
+    solutionButton.innerHTML = "Solution";
+    solutionButton.className = "top_button";
+    solutionButton.onclick = function() {
+        [1, 2, 3].forEach(function () {
+            solutionArea.appendChild(document.createElement("br"));
+        })
+        var table = document.createElement("table");
+        table.className = "board";
+        var count = 0;
+        for (var y = 0; y < 9; y++) {
+            var row = document.createElement("tr");
+            for (var x = 0; x < 9; x++) {
+                var cell = document.createElement("td");
+                var number = document.createElement("p");
+                number.className = "number";
+                number.state = sudokuSolution.charAt(count);
+                number.innerHTML = sudokuSolution.charAt(count).toString();
+                cell.appendChild(number);
+                row.appendChild(cell);
+                count++;
+            }
+            table.appendChild(row);
+        }
+        solutionArea.appendChild(table);
+    };
+    solutionArea.appendChild(solutionButton);
     this.getButtonOrNumber = function(x, y) {
         return buttons[x][y];
     };
--- js-arcade/sudoku/Sudoku.js
+++ js-arcade/sudoku/Sudoku.js
@@ -2,16 +2,16 @@ function Sudoku() {
     var thisObj = this;
     this.sudokuGame = "0000000000030000160067035004608120900090080030002079806800690350026000090000000000";
     this.sudokuSolution = "489261573235847169167935284678123945591486732342579816814692357726358491953714628";
-    var gameBoard = new GameBoardSudoku(document.getElementById("gameArea"), this.sudokuGame, function(x, y) {thisObj.click(x, y)});
+    var gameBoard = new GameBoardSudoku(document.getElementById("gameArea"), this.sudokuGame, this.sudokuSolution, function(x, y) {thisObj.click(x, y)});
     this.click = function(x, y) {
         if (typeof(gameBoard.getButtonOrNumber(x, y)) !== 'number') {
             var button = gameBoard.getButtonOrNumber(x, y);
             if (button.state === 9) {
-                button.state = 0;
                 button.innerHTML = "0";
+                button.state = 0;
             } else {
-                button.state = button.state + 1;
-                button.innerHTML = (button.state + 1).toString();   
+                button.innerHTML = (button.state + 1).toString(); 
+                button.state = button.state + 1;  
             }
         }
         if (this.isGameOver()) {
