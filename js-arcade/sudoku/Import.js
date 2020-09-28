202b099594baab4c7c9c41a47a14446ccc8e754a
Patricia Kusumaningtyas <patricia.adiyoso@gmail.com>
2020-09-28 11:27:22 -0400

    Wrote todo message to implement import page

--- js-arcade/sudoku/Import.js
+++ js-arcade/sudoku/Import.js
@@ -7,13 +7,14 @@ function ImportPage() {
     var instructions = document.createElement("p");
     instructions.id = "import_instructions";
     instructions.innerHTML = 
-    "<b>(Not implemented, please do not upload anything) </b>Import a .txt file with two strings with 81 digits each, the first representing the sudoku game and the second representing the answers. Separate them with a new line.";
+    "<b>(Feature coming soon) </b>Import a .txt file with two strings with 81 digits each, the first representing the sudoku game and the second representing the answers. Separate them with a new line.";
     importPage.appendChild(instructions);
     var inputSudoku = document.createElement("input");
     inputSudoku.type = "file";
     inputSudoku.id = "sudoku_input";
     inputSudoku.accept = "text/plain";
     importPage.appendChild(inputSudoku);
+    // TODO: implement import function
     document.getElementById("gameArea").appendChild(importPage);
 }
 
